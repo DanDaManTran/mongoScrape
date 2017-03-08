c8f003c6f63fef479b337fa29a04a479d968ae5b
DanDaManTran <DanD.Tran@yahoo.com>
2017-03-08 14:09:39 -0600

    feels great to push to a solo repo

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }))
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+require("./controllers/controllers.js")(app);
+
+app.listen(process.env.PORT || PORT);
